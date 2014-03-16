5aba17dbf1fdb4fda71eec4f0b16ab800bbb72d8
Jinfeng Ni <jni@maprtech.com>
2014-03-16 08:18:30 -0700

    Use subsumes to replace equal check in an assert check in VolcanoPlanner.

--- core/src/main/java/org/eigenbase/relopt/volcano/VolcanoPlanner.java
+++ core/src/main/java/org/eigenbase/relopt/volcano/VolcanoPlanner.java
@@ -986,7 +986,8 @@ public class VolcanoPlanner extends AbstractRelOptPlanner {
               toTrait,
               allowInfiniteCostConverters);
       if (rel != null) {
-        assert rel.getTraitSet().contains(toTrait);
+        assert rel.getTraitSet().getTrait(toTrait.getTraitDef()).
+            subsumes(toTrait);
         rel =
             completeConversion(
                 rel, allowInfiniteCostConverters, toTraits,
