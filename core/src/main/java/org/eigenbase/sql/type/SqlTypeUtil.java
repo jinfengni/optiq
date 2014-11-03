7bc87b20cc7ce42acefdf392055dda732638209c
Aman Sinha <asinha@maprtech.com>
2014-11-02 20:18:27 -0800

    DRILL-1455: Add return type-inference strategy for arithmetic operators when one of the arguments is ANY type.

--- core/src/main/java/org/eigenbase/sql/type/SqlTypeUtil.java
+++ core/src/main/java/org/eigenbase/sql/type/SqlTypeUtil.java
@@ -651,7 +651,7 @@ public abstract class SqlTypeUtil {
     }
   }
 
-  private static boolean isAny(RelDataType t) {
+  public static boolean isAny(RelDataType t) {
     return t.getFamily() == SqlTypeFamily.ANY;
   }
 
